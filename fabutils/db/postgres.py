7e5d5058a3bb831ac98b3571b95038bb0a169d33
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2012-04-25 20:24:51 -0500

    fix dependency bugs

--- fabutils/db/postgres.py
+++ fabutils/db/postgres.py
@@ -18,3 +18,43 @@ def init_db():
 	except:
 		pass
 	append("/etc/postgresql/*/main/pg_hba.conf", "local all all ident", use_sudo=True)
+
+
+def sync_psql_db(from_env_str, to_env_str):
+	"""Sync local postgres db to remote server"""
+
+	from_env = getattr(environments, from_env_str)
+	to_env = getattr(environments, to_env_str)
+
+	# work on origin server
+	from_env()
+
+	env.host_string = env.get('hosts')[0]
+	fname = "%s_%s.sql" % ( env.get('project_name'), strftime('%Y%m%d%H%M%S', gmtime() ))
+	run( "uname -r")
+	run( "pg_dump --file=/var/tmp/%s --format=p %s" % ( fname, env.get('project_name') ))
+	local( "scp %s@%s:/var/tmp/%s /var/tmp/%s" % ( env.get('user'), env.get('hosts')[0], fname, fname) )
+
+	if env.get('key_filename'):
+		local('rsync -avh -e "ssh -i %s -p %s" %s@%s:%s/uploads/* /tmp/%s-uploads' % (env.get('key_filename'), env.get('hosts')[0].split(":")[1], env.get('user'), env.get('hosts')[0].split(":")[0], env.get('project_root'), env.get('project_name')) )
+	else:
+		local('rsync -avh %s@%s:%s/uploads/* /tmp/%s-uploads' % (env.get('user'), env.get('hosts')[0], env.get('project_root'), env.get('project_name')) )
+
+	# work on destination server
+	to_env()
+
+	env.host_string = env.get('hosts')[0]
+	put("/var/tmp/%s" % fname, "/var/tmp/%s" % fname )
+	try:
+		run( "dropdb %s" % env.get('project_name'))
+	except:
+		pass
+
+	sudo("sudo -u postgres createdb -O %s %s" % (env.get('user'), env.get('db_name')))
+	run( "psql -d %s -f /var/tmp/%s" % ( env.get('project_name'), fname ))
+
+	if env.get('key_filename'):
+		local('rsync -avh -e "ssh -i %s -p %s" /tmp/%s-uploads/* %s@%s:%s/uploads' % (env.get('key_filename'), env.get('hosts')[0].split(':')[1], env.get('project_name'), env.get('user'), env.get('hosts')[0].split(':')[0], env.get('project_root')) )
+	else:
+		local("rsync -avh /tmp/%s-uploads/* %s@%s:%s/uploads" % (env.get('project_name'), env.get('user'), env.get('hosts')[0], env.get('project_root')) )
+
